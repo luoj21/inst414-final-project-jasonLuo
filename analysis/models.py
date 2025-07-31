d0797d28bed5316d1cdf646a345494726a28a3dd
luoj21 <jluo12417@gmail.com>
2025-07-31 08:25:59 -0400

    added model sketch

---
+++ analysis/models.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis 
+
+
+class DCIS_classification_model:
+    def __init__(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def fit(self):
+        pass
+    
