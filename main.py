8854e9a247c4170ace22144477ee876ac91eb93b
luoj21 <jluo12417@gmail.com>
2025-08-02 12:35:29 -0400

    extract functionality

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import pandas as pd
 import numpy as np
 
-import etl.transform
-import analysis.evaluate_model
-from vis.visualizations import *
 
+from analysis.evaluate_model import *
+from vis.visualizations import *
+from etl.extract import *
+from etl.transform import *
 
 def main(): 
     ## transform data:
-    data = etl.transform.transform_data()
+    extract_data()
+    data = transform_data()
 
     ## create and evaluate model(s)
-    analysis.evaluate_model.evaluate_model()
+    evaluate_model()
 
     ## create visualizations
     plot_class_counts(data)
     plot_ages(data)
     plot_ethnicity(data)
     plot_days_to_last_follow_up(data)
-    
 
 
 
