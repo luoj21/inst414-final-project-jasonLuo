6add397c445e2465d6056a76b2b882497a1efcaf
luoj21 <jluo12417@gmail.com>
2025-08-20 17:34:23 -0400

    try catch in extract

--- etl/extract.py
+++ etl/extract.py
@@ -14,14 +14,18 @@ def extract_data():
     """
 
     # Extract from HTAN
-    demographics = pd.read_csv('https://d13ch66cwesneh.cloudfront.net/metadata/syn39263164.csv')
-    diagnostics = pd.read_csv('https://d13ch66cwesneh.cloudfront.net/metadata/syn39263335.csv')
-    molecular_test = pd.read_csv('https://d13ch66cwesneh.cloudfront.net/metadata/syn39266384.csv')
+    try:
+        demographics = pd.read_csv('https://d13ch66cwesneh.cloudfront.net/metadata/syn39263164.csv')
+        diagnostics = pd.read_csv('https://d13ch66cwesneh.cloudfront.net/metadata/syn39263335.csv')
+        molecular_test = pd.read_csv('https://d13ch66cwesneh.cloudfront.net/metadata/syn39266384.csv')
 
-    my_logger.info('### Extracting datasets... ### \n')
-    demographics.to_csv('data/raw_data/demographics.csv', index=False)
-    diagnostics.to_csv('data/raw_data/diagnostics.csv', index = False)
-    molecular_test.to_csv('data/raw_data/molecular_test.csv', index = False)
-    my_logger.info('### Done extracting ###')
+        my_logger.info('### Extracting datasets... ### \n')
+        demographics.to_csv('data/raw_data/demographics.csv', index=False)
+        diagnostics.to_csv('data/raw_data/diagnostics.csv', index = False)
+        molecular_test.to_csv('data/raw_data/molecular_test.csv', index = False)
+        my_logger.info('### Done extracting ###')
+    
+    except:
+        print("Invalid link(s)")
 
     return demographics, diagnostics, molecular_test
